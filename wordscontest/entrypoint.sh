4799cbe9ea69435aa371e3d3c1ccd3b65d8ab387
AndreiKuznecov <ak.kuznetsow@yandex.ru>
2022-08-24 14:43:26 +0500

    fix: add email for admin

--- wordscontest/entrypoint.sh
+++ wordscontest/entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations
 python manage.py migrate
-DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_PASSWORD=admin python manage.py createsuperuser --noinput
+DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_PASSWORD=admin DJANGO_SUPERUSER_EMAIL=ak.kuznetsow@yandex.ru python manage.py createsuperuser --noinput
 python manage.py runserver 0.0.0.0:8000
